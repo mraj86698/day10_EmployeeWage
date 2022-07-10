cd6a61528bdffd395b61764863f294cc96ebe200
MohanRaj <mraj86698@gmail.com>
2022-07-10 17:44:05 +0530

    Added Total Wage for Each Company

--- src/EmpWage/EmpCompute.java
+++ src/EmpWage/EmpCompute.java
@@ -1,69 +1,65 @@
 package EmpWage;
 
 public class EmpCompute {
-	int empPresent = 1;
-	int wagePerHour = 20;
-	int fullTimeHour = 8;
-	int partTimeHour = 4;
-	double empCheck;
 
-	public EmpCompute() {
-		empCheck = Math.floor(Math.random() * 10) % 3 + 1;
+	// class constants
+	static final int partTime = 1;
+	static final int fullTime = 2;
+	// instanceConstants
+	String companyName;
+	int wagePerHour;
+	int maxWorkingDays;
+	int maxWorkingHours;
+	// instance Variable
+	int totalWage=0 ;
+
+	//creating a parameterized constructor
+
+	public EmpCompute(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+		this.companyName = companyName;
+		this.wagePerHour = wagePerHour;
+		this.maxWorkingDays = maxWorkingDays;
+		this.maxWorkingHours = maxWorkingHours;
+
 	}
 
+	int generateEmployeeType() {
+		return (int) (Math.random() * 100) % 3;
+	}
 
-
-	public void Emp(int wage_per_Hour,int FullDayHour, int PartTimeHour, int WorkingDayInMonth, int WorkingHourPerMonth ) {
-
-		final int part_Time = 2;
-		final int full_Time = 1;
-
-		int total_Wage = 0;
-
-		int dailyWages = 0;
-		int MonthlyWages = 0;
-		int workingDay = 1;
-		int day = 1;
-		int absent = 0;
-		int working_Hours = 0;
-		int totalWorkingHoursInMonth = 0;
-		//System.out.printf("%5s		%5s		%5s		%5s\n", "Day", "Working_Hours", "daily Wage","Total_Working_Hours\n\n");
-
-		for (int workingday = 1; day <= WorkingDayInMonth && totalWorkingHoursInMonth < WorkingHourPerMonth; workingday++) {
-			int empType = (int) (Math.random() * 100) % 3;
-
-			switch (empType) {
-
-			case full_Time:
-				System.out.println("Employee is Present FullTime\n");
-				working_Hours = 8;
-				dailyWages = working_Hours * wage_per_Hour;
-				day++;
-
-				break;
-
-			case part_Time:
-				System.out.println("Employee Present PartTime\n");
-				working_Hours = 4;
-				day++;
-				break;
-
-			default:
-				System.out.println("Employee is Absent\n");
-				working_Hours = 0;
-				day++;
-				break;
-
-			}
-			dailyWages = working_Hours * wage_per_Hour;
-			MonthlyWages = dailyWages + MonthlyWages;
-			totalWorkingHoursInMonth = totalWorkingHoursInMonth + working_Hours;
-
-			System.out.printf("%5d		%5d		%5d		%5d\n", day, working_Hours, dailyWages,totalWorkingHoursInMonth);
+	int getWorkingHours(int empType) {
+		switch (empType)
+		{
+		case fullTime:
+			return 8;
+		case partTime:
+			return 4;
+		default:
+			return 0;
 		}
-		System.out.println();
-		System.out.println("Total Working Hours of the Month : " + totalWorkingHoursInMonth + "Hours");
-		System.out.println();
-		System.out.println("Total wage for a month :  " + MonthlyWages);
 	}
+
+	public void calculateTotalWage() {
+		System.out.println("Computation of total wage of "+companyName+"Employee");
+		System.out.println("-----------------------------------------------------");
+		System.out.printf("%5s		%5s		%5s		%5s\n","Day","WorkingHours","Wage","TotalWorkingHours");
+		int workingHours;
+		for(int day=1,totalWorkingHours=0;day <= maxWorkingDays && totalWorkingHours <= maxWorkingHours;day++,totalWorkingHours += workingHours) {
+			int EmpType=generateEmployeeType();
+			workingHours=getWorkingHours(EmpType);
+			int wage=workingHours*wagePerHour;
+			totalWage+=wage;
+			System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHours + workingHours);
+		}
+	}
+	@Override
+	public String toString() {
+		System.out.println("Details of " + companyName + " employee");
+		System.out.println("-----------------------------------------------------");
+
+		System.out.println("Maximum working days:" + maxWorkingDays);
+		System.out.println("Maximum working hours:" + maxWorkingHours);
+		return "Total wage for a month of " + companyName + " employee is " + totalWage + "\n";
+	}
+
 }
--- src/EmpWage/EmployeeWageComputation.java
+++ src/EmpWage/EmployeeWageComputation.java
@@ -1,30 +1,18 @@
 package EmpWage;
 
-import java.util.Scanner;
-
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 
+		//creating objects and passing values
+		EmpCompute google = new EmpCompute("Google", 8, 20, 100);
+        EmpCompute microsoft = new EmpCompute("Microsoft", 4, 30, 150);
+        //calling method to display the values of object
+        google.calculateTotalWage();
+        System.out.println(google);
 
-		System.out.println("Welcome to Employee Wage Problem ");
-		System.out.println();
-		System.out.println("Shopclues Employee Data ");
-		EmpCompute shopclues = new EmpCompute();
-		shopclues.Emp(20, 8, 4, 20, 100);
-
-		System.out.println("\n Flipkart Employee Data ");
-		EmpCompute flipkart = new EmpCompute();
-		flipkart.Emp(25, 10, 5, 28, 140);
-
-		System.out.println("\n Amazon Employee Data ");
-		EmpCompute amazon = new EmpCompute();
-		amazon.Emp(28, 12, 6, 25, 180);
-
-		EmpCompute emp = new EmpCompute();
-		Scanner sc = new Scanner(System.in);
-
-
+        microsoft.calculateTotalWage();
+        System.out.println(microsoft);
 
 	}
 
