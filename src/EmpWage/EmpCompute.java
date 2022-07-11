2a950edf0d59fc48fa4c821511b3e13f30ece004
MohanRaj <mraj86698@gmail.com>
2022-07-11 14:59:23 +0530

    Added Multiple Companies to manage Employee Wage to ArrayList instead of Array

--- src/EmpWage/EmpCompute.java
+++ src/EmpWage/EmpCompute.java
@@ -1,12 +1,14 @@
 package EmpWage;
-interface IEmpCompute
-{
-    public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours);
 
-    public void calculateTotalWage();
+import java.util.ArrayList;
+
+interface IEmpCompute {
+	public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours);
+
+	public void calculateTotalWage();
 }
 
-public class EmpCompute implements IEmpCompute{
+public class EmpCompute implements IEmpCompute {
 
 	// class constants
 	static final int partTime = 1;
@@ -17,11 +19,11 @@ public class EmpCompute implements IEmpCompute{
 	int maxWorkingDays;
 	int maxWorkingHours;
 	// instance Variable
-	int totalEmpWage=0 ;
+	int totalEmpWage = 0;
 	int noOfCompanies, index;
-    EmpCompute[] companies;
+	ArrayList<EmpCompute> companies;
 
-	//creating a parameterized constructor
+	// creating a parameterized constructor
 
 	public EmpCompute(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
 		this.companyName = companyName;
@@ -30,27 +32,28 @@ public class EmpCompute implements IEmpCompute{
 		this.maxWorkingHours = maxWorkingHours;
 
 	}
-	void setTotalEmployeeWage(int totalEmpWage) {
-		this.totalEmpWage=totalEmpWage;
+
+	public EmpCompute() {
+		companies = new ArrayList<>();
 	}
+
+	void setTotalEmployeeWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
 	@Override
 	public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
-		companies[index++]=new EmpCompute(companyName,wagePerHour,maxWorkingDays,maxWorkingHours);
+
+		EmpCompute company = new EmpCompute(companyName, wagePerHour, maxWorkingDays, maxWorkingHours);
+		companies.add(company);
 	}
-	public EmpCompute(int noOfCompanies)
-    {
-        this.noOfCompanies = noOfCompanies;
-        companies = new EmpCompute[noOfCompanies];
-        index = 0;
-    }
 
 	int generateEmployeeType() {
 		return (int) (Math.random() * 100) % 3;
 	}
 
 	int getWorkingHours(int empType) {
-		switch (empType)
-		{
+		switch (empType) {
 		case fullTime:
 			return 8;
 		case partTime:
@@ -62,24 +65,26 @@ public class EmpCompute implements IEmpCompute{
 
 	@Override
 	public void calculateTotalWage() {
-		for(EmpCompute company :companies) {
-			int totalWage=calculateTotalWage(company);
+		for (EmpCompute company : companies) {
+			int totalWage = calculateTotalWage(company);
 			company.setTotalEmployeeWage(totalWage);
 			System.out.println(company);
 		}
 	}
 
 	int calculateTotalWage(EmpCompute empCompute) {
-		System.out.println("Computation of total wage of "+empCompute.companyName+"Employee");
+		System.out.println("Computation of total wage of " + empCompute.companyName + "Employee");
 		System.out.println("--------------------------------------------------------------------------------");
-		System.out.printf("%5s		%5s		%5s		%5s\n","Day","WorkingHours","Wage","TotalWorkingHours");
-		int workingHours,totalWage=0;
-		for(int day=1,totalWorkingHours=0;day <= empCompute.maxWorkingDays && totalWorkingHours <= empCompute.maxWorkingHours;day++,totalWorkingHours += workingHours) {
-			int EmpType=generateEmployeeType();
-			workingHours=getWorkingHours(EmpType);
-			int wage=workingHours* empCompute.wagePerHour;
-			totalWage+=wage;
-			System.out.printf("%10d       %10d      %10d      %10d\n", day, workingHours, wage, totalWorkingHours + workingHours);
+		System.out.printf("%5s		%5s		%5s		%5s\n", "Day", "WorkingHours", "Wage", "TotalWorkingHours");
+		int workingHours, totalWage = 0;
+		for (int day = 1, totalWorkingHours = 0; day <= empCompute.maxWorkingDays
+				&& totalWorkingHours <= empCompute.maxWorkingHours; day++, totalWorkingHours += workingHours) {
+			int EmpType = generateEmployeeType();
+			workingHours = getWorkingHours(EmpType);
+			int wage = workingHours * empCompute.wagePerHour;
+			totalWage += wage;
+			System.out.printf("%10d       %10d      %10d      %10d\n", day, workingHours, wage,
+					totalWorkingHours + workingHours);
 		}
 		return totalWage;
 	}
@@ -88,7 +93,7 @@ public class EmpCompute implements IEmpCompute{
 	public String toString() {
 		System.out.println("Details of " + companyName + " employee");
 		System.out.println("--------------------------------------------------------------------------------");
-
+		System.err.println("Wage per hour:" + wagePerHour);
 		System.out.println("Maximum working days:" + maxWorkingDays);
 		System.out.println("Maximum working hours:" + maxWorkingHours);
 		return "Total wage for a month of " + companyName + " employee is " + totalEmpWage + "\n";
--- src/EmpWage/EmployeeWageComputation.java
+++ src/EmpWage/EmployeeWageComputation.java
@@ -4,10 +4,11 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 
-		EmpCompute empCompute=new EmpCompute(3);
+		EmpCompute empCompute=new EmpCompute();
 		empCompute.addCompany("Microsoft", 4, 30, 100);
         empCompute.addCompany("Google", 5, 40, 170);
         empCompute.addCompany("Apple", 9, 10, 70);
+        empCompute.addCompany("Amazon", 19, 10, 150);
         empCompute.calculateTotalWage();
 
 
